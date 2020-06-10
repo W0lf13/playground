3a0a8bad358ca1dc73605014dc32c1258faa727b
George Berry <george.berry1@outlook.com>
2020-06-10 22:07:22 +0200

    commit

--- playground.py
+++ playground.py
@@ -1,2 +1,15 @@
 #https://github.com/W0lf13/playground
 
+#pip install kivy
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TutorialApp(App):
+    def build(self):
+        return Button(text="Hello", background_color=(0,0,1,1), font_size=150)
+
+if __name__ == "__main__":
+    print('here')
+    TutorialApp().run()
+    
